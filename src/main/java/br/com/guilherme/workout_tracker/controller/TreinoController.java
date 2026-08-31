3f921364a3f0bcfc1a45f41ec04d84513204c323
devlourenco <guilherme.gsldev@gmail.com>
2026-08-31 17:22:16 -0300

    feat: add workout controller endpoints

---
+++ src/main/java/br/com/guilherme/workout_tracker/controller/TreinoController.java
@@ -0,0 +1,38 @@
+package br.com.guilherme.workout_tracker.controller;
+
+import br.com.guilherme.workout_tracker.dto.TreinoDTO;
+import br.com.guilherme.workout_tracker.service.TreinoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/treinos")
+public class TreinoController {
+    private final TreinoService service;
+
+    public TreinoController(TreinoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<TreinoDTO> cadastrarTreino(@RequestBody TreinoDTO treinoDTO){
+       TreinoDTO treino = service.cadastrarTreino(treinoDTO);
+
+        return ResponseEntity.ok(treino);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TreinoDTO>> listarTreinos(){
+        List<TreinoDTO> treinos = service.listarTreinos();
+        return ResponseEntity.ok(treinos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TreinoDTO> listarPorId(@PathVariable Long id){
+        TreinoDTO treino = service.listarPorId(id);
+        return ResponseEntity.ok(treino);
+
+    }
+}
