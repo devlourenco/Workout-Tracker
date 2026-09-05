86c272e189c8273f19d79e4b954855601b6fe9df
devlourenco <guilherme.gsldev@gmail.com>
2026-09-05 05:23:39 -0300

    feat: handle validation errors

--- src/main/java/br/com/guilherme/workout_tracker/exceptions/RestExceptionHandler.java
+++ src/main/java/br/com/guilherme/workout_tracker/exceptions/RestExceptionHandler.java
@@ -2,8 +2,12 @@ package br.com.guilherme.workout_tracker.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -24,6 +28,21 @@ public class RestExceptionHandler {
     public ResponseEntity<String> handlerSerieNaoEncontrada(SerieNaoEncontradaException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException exception) {
+
+        Map<String, String> erros = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(erro ->
+                        erros.put(erro.getField(), erro.getDefaultMessage())
+                );
+
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
